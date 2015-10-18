074572e4954fe3ae1b8dd165152f8958a0568a51
avenezia <alessandro.venezia86@gmail.com>
2015-10-18 17:15:56 +0200

    Adding TweetTopology

---
+++ lesson2/stage6/src/jvm/udacity/storm/TweetTopology.java
@@ -0,0 +1,70 @@
+package udacity.storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+class TweetTopology
+{
+  public static void main(String[] args) throws Exception
+  {
+    TopologyBuilder builder = new TopologyBuilder();
+
+    TweetSpout tweetSpout = new TweetSpout(
+        "[customer key]",
+        "[secret key]",
+        "[access token]",
+        "[access secret]"
+    );
+
+    builder.setSpout("tweet-spout", tweetSpout, 1)
+    builder.setBolt("parse-tweet-bolt", new ParseTweetBolt(), 10).shuffleGrouping("tweet-spout");
+
+    Config conf = new Config();
+
+    conf.setDebug(true);
+
+    if (args != null && args.length > 0) {
+
+      // Run it in a live cluster
+      conf.setNumWorkers(3);
+
+      // create the topology and submit with config
+      StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+
+    } else {
+
+      // run it in a simulated local cluster
+
+      // set the number of threads to run - similar to setting number of workers in live cluster
+      conf.setMaxTaskParallelism(3);
+
+      // create the local cluster instance
+      LocalCluster cluster = new LocalCluster();
+
+      // submit the topology to the local cluster
+      cluster.submitTopology("tweet-word-count", conf, builder.createTopology());
+
+      // let the topology run for 300 seconds. note topologies never terminate!
+      Utils.sleep(300000);
+
+      // now kill the topology
+      cluster.killTopology("tweet-word-count");
+
+      // we are done, so shutdown the local cluster
+      cluster.shutdown();
+    }
+  }
+}
